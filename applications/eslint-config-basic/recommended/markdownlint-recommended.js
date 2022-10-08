1d8c236c8310f1836867d4675d868f17f15fb168
kongxiangyan <kcigaret@outlook.com>
2022-10-08 17:14:17 +0800

    feat: complete eslint-config-basic

---
+++ applications/eslint-config-basic/recommended/markdownlint-recommended.js
@@ -0,0 +1,3 @@
+// Inline from https://gitlab.com/pawelbbdrozd/eslint-plugin-markdownlint/-/blob/main/lib/index.js
+// Inline time: 2022-10-08 01:29:00
+// Latest commit: https://gitlab.com/pawelbbdrozd/eslint-plugin-markdownlint/-/commit/bb5b6151f766f212cd9937413ea549d099d60263
---
+++ applications/eslint-config-basic/tests/rules-test.md
@@ -0,0 +1,13 @@
+# Heading 1
+
+## Heading 2
+
+JavaScript, typeScript
+
+```js
+const foo = 'bar'
+
+function baz () {
+  return foo
+}
+```
