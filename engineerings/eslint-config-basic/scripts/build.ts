2e6b4efb11c3ae734bbbc9b4d90808e7e390bc90
kongxiangyan <kcigaret@outlook.com>
2022-11-20 01:19:50 +0800

    feat(eslint-config-basic): update package exports

--- engineerings/eslint-config-basic/scripts/build.ts
+++ engineerings/eslint-config-basic/scripts/build.ts
@@ -1,22 +1,60 @@
-import { writeFileSync } from 'node:fs'
+import { writeFileSync, mkdirSync } from 'node:fs'
+import { dirname } from 'node:path'
 
 import { buildSync } from 'esbuild'
 
-const code = buildSync({
-  entryPoints: ['src/main.ts'],
-  bundle: true,
-  define: {},
-  format: 'cjs',
-  minify: false,
-  outfile: 'dist/main.cjs',
-  platform: 'node',
-  splitting: false,
-  write: false
-})
-const { path, text } = code.outputFiles[0]!
+const buildConfig = (): void => {
+  const code = buildSync({
+    entryPoints: ['src/config.ts'],
+    bundle: true,
+    define: {},
+    format: 'cjs',
+    minify: false,
+    outfile: 'dist/config.cjs',
+    platform: 'node',
+    splitting: false,
+    write: false
+  })
+  const { path, text } = code.outputFiles[0]!
 
-const transformText = (text: string): string => {
-  return `${text.slice(943, -91)}\rmodule.exports = main_default;`
+  const transformText = (text: string): string => {
+    return `${text.slice(953, -91)}\rmodule.exports = config_default;`
+  }
+
+  writeFileSync(path, transformText(text))
 }
+buildConfig()
 
-writeFileSync(path, transformText(text))
+const buildIndex = (): void => {
+  const code = buildSync({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    define: {},
+    format: 'esm',
+    minify: true,
+    outfile: 'dist/index.js',
+    platform: 'node',
+    banner: {
+      js: `
+    import { createRequire } from 'module';
+    const require = createRequire(import.meta.url);
+    import path from "path";
+    import { fileURLToPath } from "url";
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    `
+    },
+    splitting: false,
+    write: false,
+    external: []
+  })
+  const { path, text } = code.outputFiles[0]!
+
+  const transformText = (text: string): string => {
+    return text
+  }
+
+  mkdirSync(dirname(path), { recursive: true })
+  writeFileSync(path, transformText(text))
+}
+buildIndex()
